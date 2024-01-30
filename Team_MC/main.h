4e24a47b1bb64e17c39882aa041a28a4536f2c56
Chukwuemeka Osuji <139784110+LyrealCurriculum@users.noreply.github.com>
2024-01-30 22:55:16 +0100

    Update main.h

--- Team_MC/main.h
+++ Team_MC/main.h
@@ -6,6 +6,15 @@
 #include <unistd.h>
 #include <limits.h>
 
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+#define S_LONG 2
+#define S_SHORT 1
+
 int _putchar(char character);
 int printf_char(va_list args, int printed);
 int printf_string(va_list args, int printed);
@@ -18,5 +27,13 @@ int printf_octal(unsigned int num, int printed);
 int printf_unsigned(unsigned int num, int printed);
 int printf_reverse(va_list args, int printed);
 int printf_pointer(va_list args, int printed);
+int printf_non_printable_string(va_list val);
+int printf_rot13(va_list args);
+int _puts(char *str);
+int _X_aux(unsigned int num);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 
-#endif
+#endif /* MAIN_H */
